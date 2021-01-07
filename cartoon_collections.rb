95fb108f44daff04d7b5d0f94bcd3cd614bd51af
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2021-01-07 01:29:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,9 +36,8 @@ def find_valid_calls(planeteer_calls)
           return item
       end
   end 
-  
   return nil
+end 
+
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-
-end 
